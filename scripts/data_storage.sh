e251c6e34ee2ff0fe1f51b5b1b1f47f23957187e
Salikhova Nazgul <nazgulka2005@gmail.com>
2025-04-25 19:51:46 +0300

    Added upload to hdfs commands

--- scripts/data_storage.sh
+++ scripts/data_storage.sh
@@ -1,2 +1,11 @@
-echo "Building project database"
-python scripts/build_projectdb.py
+# echo "Building Postgres database is starting!"
+# python scripts/build_projectdb.py
+# echo "Building Postgres database is done!"
+
+echo "Copying data into HDFS starts.."
+password=$(head -n 1 secrets/.psql.pass)
+
+sqoop list-databases --connect jdbc:postgresql://hadoop-04.uni.innopolis.ru/team15_projectdb --username team15 --password $password
+sqoop list-tables --connect jdbc:postgresql://hadoop-04.uni.innopolis.ru/team15_projectdb --username team15 --password $password
+sqoop import-all-tables --connect jdbc:postgresql://hadoop-04.uni.innopolis.ru/team15_projectdb --username team15 --password $password --compression-codec=snappy --compress --as-avrodatafile --warehouse-dir=project/warehouse --m 1
+echo "Copying data into HDFS is done!"
