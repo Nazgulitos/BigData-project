ef89db3f8c3b3c184bdb55858b0f68c5abdf6528
Salikhova Nazgul <nazgulka2005@gmail.com>
2025-05-01 14:42:04 +0300

    Added removing avsc folder before creating again

--- scripts/stage2.sh
+++ scripts/stage2.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+hdfs dfs -rm -r -skipTrash /user/team15/project/warehouse/avsc 2>/dev/null
+
 hdfs dfs -mkdir -p project/warehouse/avsc
 
 hdfs dfs -put output/*.avsc project/warehouse/avsc
@@ -7,5 +10,5 @@ password=$(head -n 1 secrets/.hive.pass)
 
 beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team15 -p $password -f sql/db.hql
 
-beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team15 -p $password -f sql/db.hql > output/hive_results.txt
+beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team15 -p $password -f sql/db.hql > ~/output/hive_results.txt
 
