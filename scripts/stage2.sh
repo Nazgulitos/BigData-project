74663b5d7de320af56ae0fe8d0e8c39b086cca0d
Chulpan Valiullina <c.valiullina@innopolis.university>
2025-05-07 13:59:36 +0300

    change way to call bash

--- scripts/stage2.sh
+++ scripts/stage2.sh
@@ -16,4 +16,5 @@ password=$(head -n 1 secrets/.hive.pass)
 
 # beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team15 -p "$password" -f sql/optimization.hql
 
-beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team15 -p $password -f scripts/eda_queries.sh
+bash scripts/run_eda_queries.sh "$password" 
+
